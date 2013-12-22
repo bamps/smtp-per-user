bf973616d9ab1ff12b0dd0a330929ac66535fe50
llenchikk <llenchikk@rambler.ru>
2013-12-22 21:49:08 +0400

    update structure

---
+++ smtp_per_user/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "smtp_per_user",
+    'version': '0.1',
+    'category': 'Mail',
+    'description': """Using different smptp settings for user's outgoing emails""",
+    'author': 'llenchikk <llenchikk@rambler.ru>',
+    'license': 'AGPL-3',
+    "depends" : ['base', 'mail'],
+    'data': ["smtp_per_user_view.xml"],
+    "active": False,
+    "installable": True
+}
